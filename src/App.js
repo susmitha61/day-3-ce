d0eb6493953008ce2220fcb4ec05cde5b2726ef7
susmitha <108916471+susmitha61@users.noreply.github.com>
2023-04-05 10:13:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MusicWorld from "./Components/MusicWorld"
+
+export class App extends React.Component {
+    render()
+    {
+        return (
+        <div>
+        <MusicWorld/>
+        </div>
+        );
+    }
+}
+
+export default App;
